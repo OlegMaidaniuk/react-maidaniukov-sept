1fdea7211e40df823f5f0c492a75d5867e7095c0
OlegMaidaniuk <omaydanuk@gmail.com>
2023-01-28 12:14:35 +0200

    12 Komit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+// import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+app lesson1 1
+        </div>
+    );
+};
+
+export {App};
+
+
+
+
+
+
+// import './App.css'
+// import Simpsoncomponent from "./components/Simpsoncomponent";
+//
+// function App() {
+//
+//     return (
+//         <div className="wrap">
+//
+//             <Simpsoncomponent
+//                 itemName={'bart'}
+//                 pic={'https://toppng.com/uploads/preview/simson-11551061369fryehlpudf.png'}
+//             />
+//             <Simpsoncomponent
+//                 itemName={'homer'}
+//                 pic={'https://images.immediate.co.uk/production/volatile/sites/3/2018/08/Simpsons_SO28_Gallery_11-fb0b632.jpg'}
+//             />
+//             <Simpsoncomponent
+//                 itemName={'Lisa'}
+//                 pic={'https://upload.wikimedia.org/wikipedia/uk/thumb/a/ae/Lisasmiling.png/320px-Lisasmiling.png'}
+//             />
+//             <Simpsoncomponent
+//                 itemName={'Marge'}
+//                 pic={'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}
+//             />
+//
+//         </div>
+//
+//
+//     )
+//
+// }
+//
+// export {
+//     App
+// };
