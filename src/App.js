2d8e1eff8ec3e66381af6d4b52f1d8af8702f2a1
OlegMaidaniuk <omaydanuk@gmail.com>
2023-01-29 16:31:40 +0200

    123 Komit .idea-1 1

--- src/App.js
+++ src/App.js
@@ -1,52 +1,15 @@
 import React from 'react';
+
 const App = () => {
     return (
         <div>
-            <App name ={'Marge'} surname={'Simpson'} size={'45'} image={'https://i.discogs.com/WI38UGun7TKKNg04MTHK0yV-I1Yb75sdJ_t0GEermmo/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTI2MzMx/MzAtMTMzNTcyMDM5/MC5wbmc.jpeg'} age={'46'}/>
-            <App name={'Homer'} surname={'Simpson'} size={'47'} image={'https://images.prom.ua/2472016152_w640_h640_kruzhka-s-printom.jpg'}age={'48'}/>
+            <App name={'Marge'} surname={'Simpson'} size={'45'}
+                 image={'https://i.discogs.com/WI38UGun7TKKNg04MTHK0yV-I1Yb75sdJ_t0GEermmo/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTI2MzMx/MzAtMTMzNTcyMDM5/MC5wbmc.jpeg'}
+                 age={'46'}/>
+            <App name={'Homer'} surname={'Simpson'} size={'47'}
+                 image={'https://images.prom.ua/2472016152_w640_h640_kruzhka-s-printom.jpg'}
+                 age={'48'}/>
         </div>
     );
 };
-
 export {App};
-
-
-
-
-
-
-// import './App.css'
-// import Simpsoncomponent from "./components/Simpsoncomponent";
-//
-// function App() {
-//
-//     return (
-//         <div className="wrap">
-//
-//             <Simpsoncomponent
-//                 itemName={'bart'}
-//                 pic={'https://toppng.com/uploads/preview/simson-11551061369fryehlpudf.png'}
-//             />
-//             <Simpsoncomponent
-//                 itemName={'homer'}
-//                 pic={'https://images.immediate.co.uk/production/volatile/sites/3/2018/08/Simpsons_SO28_Gallery_11-fb0b632.jpg'}
-//             />
-//             <Simpsoncomponent
-//                 itemName={'Lisa'}
-//                 pic={'https://upload.wikimedia.org/wikipedia/uk/thumb/a/ae/Lisasmiling.png/320px-Lisasmiling.png'}
-//             />
-//             <Simpsoncomponent
-//                 itemName={'Marge'}
-//                 pic={'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}
-//             />
-//
-//         </div>
-//
-//
-//     )
-//
-// }
-//
-// export {
-//     App
-// };
--- src/components/Simpson/Simpson.js
+++ src/components/Simpson/Simpson.js
@@ -8,14 +8,6 @@ const Simpson = (props) => {
             <div>size:{size}</div>
             <img src={image} alt={name}/>
             <div>age:{age}</div>
-
-            {/*<div>name Homer</div>*/}
-            {/*<div>surname Simpson</div>*/}
-            {/*<img src="https://images.prom.ua/2472016152_w640_h640_kruzhka-s-printom.jpg" alt="homer"/>*/}
-
-            {/*<div>name Homer</div>*/}
-            {/*<div>surname Simpson</div>*/}
-            {/*<img src="https://images.prom.ua/2472016152_w640_h640_kruzhka-s-printom.jpg" alt="homer"/>*/}
         </div>
     );
 };
